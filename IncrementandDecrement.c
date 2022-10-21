bc3fa97bd030c1426eefffe65b6643596de48d26
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2022-10-21 16:28:51 +0530

    [ADDED]: Increment and Decrement of Numbers

---
+++ IncrementandDecrement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("\nEnter the Number of A :");
+	scanf("%d",&a);
+	printf("\nEnter the Number of B :");
+	scanf("%d",&b);
+	a++;
+	printf("\nIncrement of A:%d",a++);
+	b--;
+	printf("\nDecrement of B:%d",b--);
+	return 0;
+}
+	
+	
