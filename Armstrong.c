536270053ec1086c67a8ebb4ba9c1bb966749bc4
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2022-11-01 18:46:04 +0530

    [ADDED]Bhavesh:The Armstrong Number by Using The While Loop

---
+++ Armstrong.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int no,r,sum=0,temp=0;
+	printf("Enter the Number:");
+	scanf("%d",&no);
+	temp=no;
+	while(no>0)
+	{
+		r=no%10;
+		sum=sum+(r*r*r);
+		no=no/10;
+	}
+	if(temp==sum)
+	{
+		printf("\nThe Number is Armstrong Number");
+	}
+	else
+	{
+		printf("\nThe Number is not Armstrong Number");
+	}
+	return 0;
+}
