ffcf4bf345a29f5381df5f529b5eac1507bdfcf2
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2022-11-03 21:21:15 +0530

    [ADDED]Bhavesh:Finding Odd-Even by using the goto Statement

---
+++ GotoStatement.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter a Positive Integer:");
+	scanf("%d",&a);
+	if(a%2==0)
+	  goto Even;
+	else
+	  goto Odd;
+	Even :
+		printf("Number is Even\n");
+	exit(0);
+	Odd:
+		printf("Number is Odd\n");
+	return 0;
+}
