93a67644d02cda5131f089b4c316bd66b3fc5380
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2022-11-05 20:29:46 +0530

    [ADDED]Bhavesh:The Introduction made by using The Function(Call by Value)

---
+++ Introduction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void introduction()
+{
+	printf("\nHi");
+	printf("\nMy Name is Bhavesh Patil");
+	printf("\nI am A Full Stack Developer");
+}
+int main()
+{
+	introduction();
+	return 0;
+}
