30c006840aaf3effa7abb913e9a69ec6167bcaff
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2022-11-05 20:28:11 +0530

    [ADDED]Bhavesh:The Arithmatic Operations by using The Function(Call by Value)

---
+++ FunArithmatic.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("Enter the Numbers:");
+	scanf("%d %d",&a,&b);
+	add(a,b);
+	sub(a,b);
+	mult(a,b);
+	div(a,b);
+}
+void add(int a,int b)
+{
+	printf("\nAddition:%d",a+b);
+}
+void sub(int a,int b)
+{
+	printf("\nSubtraction:%d",a-b);
+}
+void mult(int a,int b)
+{
+	printf("\nMultiplication:%d",a*b);
+}
+void div(int a,int b)
+{
+	printf("\nDivision:%d",a/b);
+}
